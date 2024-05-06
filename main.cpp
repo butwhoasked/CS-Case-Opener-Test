d89d845a7ef020eb09b87b6a46c03bd8bf743886
butwhoasked <86532379+butwhoasked@users.noreply.github.com>
2024-05-06 19:37:19 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,10 +7,29 @@ bool win = false;
 
 int main(void);
 {
-  bool gold = false, red = false, pink = false, purple = false, blue = false;
+  bool bgold = false, bred = false, bpink = false, bpurple = false, bblue = false;
+  int wgold = 
+  int varrepeat = 0;
+
+  //random generator for cases
+  random_device rd;
+	mt19937 generator(rd());
+	uniform_int_distribution<>casewin(1, 10000);
+
+  do {
+     cout << "Case Simulator" << endl << endl;
+
+    
 
 
+     
+     cout  << "Skin rarity: " << 
 
+    
 
+       cout << "\n\nAnother Case? (Y)es/(N)o" << endl;
+       varrepeat = _getch()
+        system("cls")
+       } while (varrepeat != 'n' && varrepeat != 'N');
   return 0;
 }
