1b732e7f4fba02933b35721ae6d2299237dd396c
butwhoasked <86532379+butwhoasked@users.noreply.github.com>
2024-05-06 19:26:41 +0200

    Create main.cpp
    main file

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+using namespace std;
+
+bool win = false;
+
+int main(void);
+{
+  bool gold = false, red = false, pink = false, purple = false, blue = false;
+
+
+
+
+  return 0;
+}
