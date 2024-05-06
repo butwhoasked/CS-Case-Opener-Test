e950c1cda4dc59d020ee6ef0c78e65f8ed818124
butwhoasked <86532379+butwhoasked@users.noreply.github.com>
2024-05-06 19:42:45 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,31 +3,30 @@
 #include <windows.h>
 using namespace std;
 
-bool win = false;
-
 int main(void);
 {
   bool bgold = false, bred = false, bpink = false, bpurple = false, bblue = false;
   int cpull = 0;
   int varrepeat = 0;
 
-  //random generator for cases
+  //Random generator for cases
   random_device rd;
 	mt19937 generator(rd());
 	uniform_int_distribution<>casewin(1, 10000);
 
+//Start of the loop
   do {
      cout << "Case Simulator" << endl << endl;
      casewin = cpull;
-
+//Chooses which number between 1 and 10000 is chosen
 	if (casewin = 
 
 
-		
+//Displays what rarity the pulled skin is
      cout  << "Skin rarity: " << 
 
     
-
+//Asks the user if he wants to open another one
        cout << "\n\nAnother Case? (Y)es/(N)o" << endl;
        varrepeat = _getch()
         system("cls")
